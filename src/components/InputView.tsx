294633ed59a2954283cd3b68f5f32193a3a957f0
gunwoongPark <bung1438@gmail.com>
2022-11-23 01:35:31 +0900

    feat: 검색어 debounce 전 input value setting 로직 추가 및 queryString으로 데이터 유지

--- src/components/InputView.tsx
+++ src/components/InputView.tsx
@@ -1,22 +1,39 @@
 import { debounce } from "lodash";
-import { ChangeEvent } from "react";
-import { useNavigate } from "react-router-dom";
+import { ChangeEvent, useEffect, useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const InputView = () => {
+  // navigate
   const navigate = useNavigate();
+  // searchParams
+  const searchParams = useSearchParams()[0];
 
+  const [searchInput, setSearchInput] = useState<string>("");
+
+  // useEffect
+  useEffect(() => {
+    const keyword = searchParams.get("keyword");
+    if (typeof keyword === "string") {
+      setSearchInput(keyword);
+    }
+  }, [searchParams]);
+
+  // function
   const debounceOnChange = debounce((e) => {
     if (!!e.target.value.length) {
       navigate({ pathname: "/search", search: `?keyword=${e.target.value}` });
     } else {
       navigate({ pathname: "/" });
     }
-  }, 750);
+  }, 1000);
   const onChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value);
     debounceOnChange(e);
   };
 
-  return <input type="text" onChange={(e) => onChangeInput(e)} />;
+  return (
+    <input type="text" value={searchInput} onChange={(e) => onChangeInput(e)} />
+  );
 };
 
 export default InputView;
