2cf654efaed09a9337ca01d8a40b76f2eee31e5b
gunwoongPark <bung1438@gmail.com>
2022-12-19 03:53:09 +0900

    feat: 프로젝트 생성

--- src/lib/api/video/schema.d.ts
+++
@@ -1,9 +0,0 @@
-import { ChartType, PartType } from "../../../types/type";
-
-export interface VideoReq {
-  part: PartType;
-  chart: ChartType;
-  maxResults: number;
-  pageToken?: string;
-  regionCode: string;
-}
--- src/lib/config.ts
+++
@@ -1,5 +0,0 @@
-class Config {
-  apiBaseUri = process.env.REACT_APP_YOUTUBE_API_BASE_URL;
-}
-
-export const config = new Config();
